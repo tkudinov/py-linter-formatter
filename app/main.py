5d585e0e28e80cf0fb233ede6eb4657426e6bdf3
Taras <t.kudinov90@gmail.com>
2024-11-18 15:58:28 +0200

    Made a format_linter_error function call inside format_linter_report body.

--- app/main.py
+++ app/main.py
@@ -28,13 +28,7 @@ def format_linter_report(linter_report: dict) -> list:
     return [
         {
             "errors": [
-                {
-                    "line": error["line_number"],
-                    "column": error["column_number"],
-                    "message": error["text"],
-                    "name": error["code"],
-                    "source": "flake8"
-                } for error in errors
+                format_linter_error(error) for error in errors
             ],
             "path": path,
             "status": "failed" if len(errors) > 0 else "passed"
